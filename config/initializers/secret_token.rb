6c0177614063cc02356843b0686fb50b5adcf70a
Jvbelikov <jury@cookie-mac.local>
2013-11-09 12:59:17 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Loanittome::Application.config.secret_key_base = 'c6c04aab33816d4b3723f7567009afad0fedb5eeb4b4738bda68b9c3af027d528099f8c16d7f60093acdd9fb74ba49851356f304792501dc70d17ad3e2318b32'
